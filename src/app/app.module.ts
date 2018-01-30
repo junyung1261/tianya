0b6e4dcec637efb06ca935f5461fef001102f0f5
junyung1261 <junyung1261@naver.com>
2018-01-30 19:36:37 +0900

    tt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -71,11 +71,11 @@ export function provideSettings(storage: Storage) {
 //********** firebase configuration  ************ */
 export const config = { 
   apiKey: "AIzaSyCj9s5Z1ZKrff_4BxF1ZSryzjY8KzTmbtI",
-    authDomain: "tianya-6d56d.firebaseapp.com",
-    databaseURL: "https://tianya-6d56d.firebaseio.com",
-    projectId: "tianya-6d56d",
-    storageBucket: "",
-    messagingSenderId: "286006030163"
+  authDomain: "tianya-6d56d.firebaseapp.com",
+  databaseURL: "https://tianya-6d56d.firebaseio.com",
+  projectId: "tianya-6d56d",
+  storageBucket: "tianya-6d56d.appspot.com",
+  messagingSenderId: "286006030163"
 };
 
 @NgModule({

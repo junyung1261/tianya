de031295b2b6c9fb5a2e7f99dbb5d88db42d5501
Junho Seo <stilllove64@gmail.com>
2018-02-06 19:00:15 +0900

    20180206

--- src/pages/heart-charge/heart-charge.module.ts
+++ src/pages/heart-charge/heart-charge.module.ts
@@ -4,12 +4,7 @@ import { HeartChargePage } from './heart-charge';
 
 
 @NgModule({
-  declarations: [
-    HeartChargePage,
-    
-  ],
-  imports: [
-    IonicPageModule.forChild(HeartChargePage)
-  ],
+  declarations: [ HeartChargePage ],
+  imports: [ IonicPageModule.forChild(HeartChargePage) ]
 })
 export class HeartChargePageModule {}
--- src/pages/heart-charge/heart-charge.ts
+++ src/pages/heart-charge/heart-charge.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ModalController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'heart-charge',
+  selector: 'page-heart-charge',
   templateUrl: 'heart-charge.html',
 })
 export class HeartChargePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public modalCtrl:ModalController) {
   }
 
   ionViewDidLoad() {
@@ -16,6 +16,17 @@ export class HeartChargePage {
   }
 
  
-  
+  presentHeartHistory(){
+    let viewHistory = this.modalCtrl.create('HeartHistoryPage',  {
+      enterAnimation: 'modal-slide-in',
+      leaveAnimation: 'modal-slide-out'
+    });
+    viewHistory.onDidDismiss(data => {
+      if(data){
+        this.ionViewDidLoad();
+      }
+    });
+    viewHistory.present();
+  }
 
 }
